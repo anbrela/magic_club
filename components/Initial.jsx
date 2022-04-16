12a5bf2ce84c64a8b34a459bb88621f7e5a1137f
Pablo Naveira <pablonaveiralopez@gmail.com>
2022-04-16 13:24:25 +0200

    initial test

---
+++ components/Initial.jsx
@@ -0,0 +1,39 @@
+export const Initial = () => {
+  return (
+    <div className="h-full flex flex-col justify-center w-full min-h-screen background p-10 space-y-2">
+      <p className="text-white text-gray-300 text-sm">Querido Arthur.</p>
+
+      <p className="text-white text-gray-300 text-sm">
+        Está a punto de comenzar otro año más y todo está descontrolado. La
+        oscuridad <span className="noselect">rebosa</span> en todas las esquinas
+        del castillo. Tendremos más <span className="noselect">aurores </span>{" "}
+        controlando las entradas y salidas pero me temo que quizás necesitamos
+        su ayuda.
+      </p>
+
+      <p className="text-white text-gray-300 text-sm">
+        Gracias a su maravillosa labor quizás podamos seguir{" "}
+        <span className="noselect">haciendo </span> frente a todo lo que nos
+        amedrenta. Hemos sufrido muchos{" "}
+        <span className="noselect"> ataques </span> y estamos exhaustos. Espero
+        que con esta carta, sea capaz <span className="noselect">de </span>{" "}
+        contactar.
+      </p>
+
+      <p className="text-white text-gray-300 text-sm">
+        De no ser así, absténgase de intentarlo por{" "}
+        <span className="noselect">otras </span> vías{" "}
+        <span className="noselect">o </span> canales ya que sabré que{" "}
+        <span className="noselect"> usted </span> no es el verdadero Arthur
+        Weasley.
+      </p>
+
+      <p className="text-white text-gray-300 text-sm">
+        Está lucha lo es <span className="noselect">todo. </span> Es lo único
+        que <div className="effect">cuenta.</div>
+      </p>
+
+      <p className="text-white text-gray-300 text-sm">Att. Ms Ferret</p>
+    </div>
+  );
+};
